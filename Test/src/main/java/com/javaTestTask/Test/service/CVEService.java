bd964f9f320ba90564357ccda45e053822620318
alex <alex@susanu.com>
2021-07-04 15:58:31 +0100

    added file service

--- Test/src/main/java/com/javaTestTask/Test/service/CVEService.java
+++ Test/src/main/java/com/javaTestTask/Test/service/CVEService.java
@@ -16,26 +16,35 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.Year;
 import java.time.ZoneId;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class CVEService {
-    File file = new File("CVE.json");
-    @Test
-    public void readFile() throws ParseException {
-        ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-        List<CVE_Items> cveItemsList = new ArrayList<>();
-        try{
-            CVE cveMapper = objectMapper.readValue(file, CVE.class);
-            for(CVE_Items c : cveMapper.getCVE_Items()){
-                cveItemsList.add(new CVE_Items(c.getCveType(), c.getImpact(), c.getPublishedDate()));
+    FileService fileService = new FileService();
+    List<CVE_Items> cveItemsList = fileService.readFile();
+
+    private List<Integer> yearList(List<CVE_Items> cveItemsList) {
+        List<Integer> integerArrayList = new ArrayList<>();
+        cveItemsList.forEach(y -> {
+            try {
+                integerArrayList.add(parseDate(y.getPublishedDate()).getYear());
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
+        return integerArrayList;
+    }
+
+    /**
+     *
+     * @param cveItemsList
+     * @return total number of CVE per month for all years
+     * @throws ParseException
+     */
+    private Map<Month, Integer> cvePerMonth(List<CVE_Items> cveItemsList) throws ParseException {
         Map<Month, Integer> perMonth = new HashMap<>();
         int noCVE = 0;
         for(CVE_Items c : cveItemsList){
@@ -48,11 +57,42 @@ public class CVEService {
             }
             perMonth.put(month, noCVE);
         }
-        for(Map.Entry<Month, Integer> entry : perMonth.entrySet()){
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
+        //sort month
+        perMonth = perMonth.entrySet().stream()
+                           .sorted(Map.Entry.comparingByKey())
+                           .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, n) -> o, LinkedHashMap::new));
+        return perMonth;
     }
 
+
+    /**
+     * @param cveItemsList
+     * @return no of CVE per month for each year
+     * @throws ParseException
+     */
+    private Map<Integer, Map<Month, Integer>> cvePerMonthPerYear(List<CVE_Items> cveItemsList) throws ParseException {
+        Map<Integer, Map<Month, Integer>> perMonthPerYear = new HashMap<>();
+        List<Integer> years = yearList(cveItemsList);
+        years.stream().forEach(y -> {
+            try {
+                perMonthPerYear.put(y,cvePerMonth(cveItemsList));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        });
+        //sort year
+        return perMonthPerYear.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, n) -> o, LinkedHashMap::new));
+    }
+
+
+    /**
+     * parse string into date
+     * @param dateFromFile string date
+     * @return LocalDate date
+     * @throws ParseException
+     */
     private LocalDate parseDate(String dateFromFile) throws ParseException {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
         Date date = new Date();
---
+++ Test/src/main/java/com/javaTestTask/Test/service/FileService.java
@@ -0,0 +1,35 @@
+package com.javaTestTask.Test.service;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javaTestTask.Test.CVE;
+import com.javaTestTask.Test.CVE_Items;
+import org.junit.Test;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FileService {
+    private File file = new File("CVE.json");
+
+    public List<CVE_Items> readFile(){
+        ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        List<CVE_Items> cveItemsList = new ArrayList<>();
+        try{
+            CVE cveMapper = objectMapper.readValue(file, CVE.class);
+            for(CVE_Items c : cveMapper.getCVE_Items()){
+                cveItemsList.add(new CVE_Items(c.getCveType(), c.getImpact(), c.getPublishedDate()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cveItemsList;
+    }
+}
